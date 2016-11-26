f3bd5f8dce43db82cd8e4965dd90b699dac95635
Pawin Thepbanchaporn <w.i.n@me.com>
2016-11-27 01:25:40 +0700

    Initial Commit

---
+++ findji/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  findji
+//
+//  Created by win on 11/27/16.
+//  Copyright © 2016 Pawin Thepbanchaporn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
